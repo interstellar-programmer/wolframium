6e13d5b64b1fcc9a4cffdebef938b9d09f1d8baa
interstellar-programmer <interstellar.programmer@gmail.com>
2024-04-04 23:50:39 +0500

    The first lines of Wolframium Library code

---
+++ main.py
@@ -0,0 +1,25 @@
+import wolframium
+
+# print("Hello from Wolframium library!")
+
+# print(wolframium.lmfn_add_2_args(1.1, 1.1))
+# print(wolframium.lmfn_add_3_args(1.1, 1.1, 1.1))
+# print(wolframium.lmfn_add_4_args(1.1, 1.1, 1.1, 1.1))
+# print(wolframium.lmfn_add_5_args(1.1, 1.1, 1.1, 1.1, 1.1))
+# print(wolframium.lmfn_add_6_args(1.1, 1.1, 1.1, 1.1, 1.1, 1.1))
+# print(wolframium.lmfn_add_7_args(1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1))
+# print(wolframium.lmfn_add_8_args(1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1))
+
+# print(wolframium.add_2_int_args(4.4, 8.8))
+# print(wolframium.add_3_int_args(4.4, 8.8, 7.7))
+# print(wolframium.add_4_int_args(4.4, 8.8, 7.7, 4.4))
+# print(wolframium.add_5_int_args(1.1, 2.2, 1.1, 2.2, 1.1))
+# print(wolframium.add_6_int_args(1.1, 2.2, 1.1, 2.2, 1.1, 2.2))
+# print(wolframium.add_7_int_args(1.1, 2.2, 1.1, 2.2, 1.1, 2.2, 3.3))
+# print(wolframium.add_8_int_args(1.1, 2.2, 1.1, 2.2, 1.1, 2.2, 3.3, 4.4))
+
+
+# print(wolframium.get_n_natural_even_numbers_in_a_row(4))
+# print(wolframium.get_n_natural_odd_numbers_in_a_row(4))
+# print(wolframium.get_sum_of_the_first_n_members_of_arithmetical_progression(24, 4))
+# print(wolframium.get_row_of_the_first_n_members_of_arithmetical_progression(25, 4))
---
+++ wolframium.py
@@ -0,0 +1,188 @@
+# =============================================================================================================================
+# ARITHMETICS =================================================================================================================
+# =============================================================================================================================
+
+# addition #
+
+# Sum of two integer or float arguments
+lmfn_add_2_args = lambda arg1, arg2 : arg1 + arg2
+
+# Sum of three integer or float arguments
+lmfn_add_3_args = lambda arg1, arg2, arg3 : arg1 + arg2 + arg3
+
+# Sum of four integer or float arguments
+lmfn_add_4_args = lambda arg1, arg2, arg3, arg4 : arg1 + arg2 + arg3 + arg4
+
+# Sum of five integer or float arguments
+lmfn_add_5_args = lambda arg1, arg2, arg3, arg4, arg5 : arg1 + arg2 + arg3 + arg4 + arg5
+
+# Sum of six integer or float arguments
+lmfn_add_6_args = lambda arg1, arg2, arg3, arg4, arg5, arg6 : arg1 + arg2 + arg3 + arg4 + arg5 + arg6
+
+# Sum of seven integer or float arguments
+lmfn_add_7_args = lambda arg1, arg2, arg3, arg4, arg5, arg6, arg7 : arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7
+
+# Sum of eight integer or float arguments
+lmfn_add_8_args = lambda arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 : arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8
+
+# Integer sum of two integer arguments
+def add_2_int_args(arg1:int,
+                   arg2:int)->int:
+    return (int(arg1) +
+            int(arg2))
+
+# Integer sum of three integer arguments
+def add_3_int_args(arg1:int,
+                   arg2:int,
+                   arg3:int)->int:
+    return (int(arg1) +
+            int(arg2) +
+            int(arg3))
+
+# Integer sum of four integer arguments
+def add_4_int_args(arg1:int,
+                   arg2:int,
+                   arg3:int,
+                   arg4:int)->int:
+    return (int(arg1) +
+            int(arg2) +
+            int(arg3) +
+            int(arg4))
+
+# Integer sum of five integer arguments
+def add_5_int_args(arg1:int,
+                   arg2:int,
+                   arg3:int,
+                   arg4:int,
+                   arg5:int)->int:
+    return (int(arg1) +
+            int(arg2) +
+            int(arg3) +
+            int(arg4) +
+            int(arg5))
+
+# Integer sum of six integer arguments
+def add_6_int_args(arg1:int,
+                   arg2:int,
+                   arg3:int,
+                   arg4:int,
+                   arg5:int,
+                   arg6:int)->int:
+    return (int(arg1) +
+            int(arg2) +
+            int(arg3) +
+            int(arg4) +
+            int(arg5) +
+            int(arg6))
+
+# Integer sum of seven integer arguments
+def add_7_int_args(arg1:int,
+                   arg2:int,
+                   arg3:int,
+                   arg4:int,
+                   arg5:int,
+                   arg6:int,
+                   arg7:int)->int:
+    return (int(arg1) +
+            int(arg2) +
+            int(arg3) +
+            int(arg4) +
+            int(arg5) +
+            int(arg6) +
+            int(arg7))
+
+# Integer sum of eight integer arguments
+def add_8_int_args(arg1:int,
+                   arg2:int,
+                   arg3:int,
+                   arg4:int,
+                   arg5:int,
+                   arg6:int,
+                   arg7:int,
+                   arg8:int)->int:
+    return (int(arg1) +
+            int(arg2) +
+            int(arg3) +
+            int(arg4) +
+            int(arg5) +
+            int(arg6) +
+            int(arg7) +
+            int(arg8))
+
+# .................................
+
+# subtraction #
+
+lmfn_subt_2_args = lambda arg1, arg2 : arg1 - arg2
+lmfn_subt_3_args = lambda arg1, arg2, arg3 : arg1 - arg2 - arg3
+lmfn_subt_4_args = lambda arg1, arg2, arg3, arg4 : arg1 - arg2 - arg3 - arg4
+lmfn_subt_5_args = lambda arg1, arg2, arg3, arg4, arg5 : arg1 - arg2 - arg3 - arg4 - arg5
+lmfn_subt_6_args = lambda arg1, arg2, arg3, arg4, arg5, arg6 : arg1 - arg2 - arg3 - arg4 - arg5 - arg6
+lmfn_subt_7_args = lambda arg1, arg2, arg3, arg4, arg5, arg6, arg7 : arg1 - arg2 - arg3 - arg4 - arg5 - arg6 - arg7
+lmfn_subt_8_args = lambda arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 : arg1 - arg2 - arg3 - arg4 - arg5 - arg6 - arg7 - arg8
+
+
+# multiplication #
+
+lmfn_mult_2_args = lambda arg1, arg2 : arg1 * arg2
+lmfn_mult_3_args = lambda arg1, arg2, arg3 : arg1 * arg2 * arg3
+lmfn_mult_4_args = lambda arg1, arg2, arg3, arg4 : arg1 * arg2 * arg3 * arg4
+lmfn_mult_5_args = lambda arg1, arg2, arg3, arg4, arg5 : arg1 * arg2 * arg3 * arg4 * arg5
+lmfn_mult_6_args = lambda arg1, arg2, arg3, arg4, arg5, arg6 : arg1 * arg2 * arg3 * arg4 * arg5 * arg6
+lmfn_mult_7_args = lambda arg1, arg2, arg3, arg4, arg5, arg6, arg7 : arg1 * arg2 * arg3 * arg4 * arg5 * arg6 * arg7
+lmfn_mult_8_args = lambda arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 : arg1 * arg2 * arg3 * arg4 * arg5 * arg6 * arg7 * arg8
+
+
+# division #
+
+lmfn_div_2_args = lambda arg1, arg2 : arg1 / arg2
+lmfn_div_3_args = lambda arg1, arg2, arg3 : arg1 / arg2 / arg3
+lmfn_div_4_args = lambda arg1, arg2, arg3, arg4 : arg1 / arg2 / arg3 / arg4
+lmfn_div_5_args = lambda arg1, arg2, arg3, arg4, arg5 : arg1 / arg2 / arg3 / arg4 / arg5
+lmfn_div_6_args = lambda arg1, arg2, arg3, arg4, arg5, arg6 : arg1 / arg2 / arg3 / arg4 / arg5 / arg6
+lmfn_div_7_args = lambda arg1, arg2, arg3, arg4, arg5, arg6, arg7 : arg1 / arg2 / arg3 / arg4 / arg5 / arg6 / arg7
+lmfn_div_8_args = lambda arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 : arg1 / arg2 / arg3 / arg4 / arg5 / arg6 / arg7 / arg8
+
+
+# # # # # # # # # #
+# usual functions #
+# # # # # # # # # #
+
+# sequences and rows #
+
+# 2n
+def get_n_natural_even_numbers_in_a_row(n):
+    list = []
+    for num in range(n):
+        list.append(2 * num)
+    return list
+
+# 2n + 1
+def get_n_natural_odd_numbers_in_a_row(n):
+    list = []
+    for num in range(n):
+        list.append(2 * num + 1)
+    return list
+
+# PROGRESSIONS
+
+# arithmetical progression
+
+# the sum of the first n-members of arithmetical progression
+def get_sum_of_the_first_n_members_of_arithmetical_progression(a_n, d):
+    return ((d * 1 + d * a_n) * a_n) / 2
+
+# the row of the first n-members of arithmetical progression
+def get_row_of_the_first_n_members_of_arithmetical_progression(a_n, d):
+    list = []
+    iter = 0
+    for a in range(a_n):
+        iter += d
+        list.append(iter)
+    return list
+
+
+
+# 4
+
+# class Wolframium:
